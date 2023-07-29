73e1c13f77b795ce99843987e974ecb6c572188e
Gavin Pease <gavinpease@gmail.com>
2023-07-28 22:58:53 -0600

    Add more items

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ height_scale = 0.2
 item_w = 77
 item_h = 77
 
-height = ss.shape[1]
-width = ss.shape[0]
+height = ss.shape[0]
+width = ss.shape[1]
 
-method = "cv2.TM_CCORR_NORMED"
+print(width, height)
+
+method = "cv2.TM_CCOEFF_NORMED"
 
 # crop image so that the last 30% horizontally and the middle 20% vertically are used
-#ss_cropped = ss[540:640, 1550:1910]
-ss_cropped = ss
-items = list(pathlib.Path("items").glob("*.png"))
+ss_cropped = ss[540:640, 1550:1910]
+# ss_cropped = ss
 
+items = list(pathlib.Path("items").glob("*.png"))
 found_items = []
 for _item in items:
     item = cv2.imread(str(_item))
     res = cv2.matchTemplate(ss_cropped, item, eval(method))
-    loc = np.where(res >= 0.95)
+    loc = np.where(res >= 0.70)
     if not np.any(loc):
         continue
     (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(res)
@@ -35,5 +37,7 @@ for _item in items:
 
 found_items.sort()
 print(found_items)
-cv2.imshow("test", ss_cropped)
+
+cv2.imshow("RotMG Items", ss_cropped)
 cv2.waitKey()
+# return found_items
