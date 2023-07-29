d2c2533b3378ee059a5626c2e16f92781697da0d
Gavin Pease <gavinpease@gmail.com>
2023-07-28 21:21:59 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import cv2
+import pathlib
+import numpy as np
+ss = cv2.imread("/Users/gcpease/Downloads/image.png")
+
+width_scale = 0.6
+height_scale = 0.2
+
+item_w = 77
+item_h = 77
+
+height = ss.shape[1]
+width = ss.shape[0]
+
+method = "cv2.TM_CCORR_NORMED"
+
+# crop image so that the last 30% horizontally and the middle 20% vertically are used
+#ss_cropped = ss[540:640, 1550:1910]
+ss_cropped = ss
+items = list(pathlib.Path("items").glob("*.png"))
+
+found_items = []
+for _item in items:
+    item = cv2.imread(str(_item))
+    res = cv2.matchTemplate(ss_cropped, item, eval(method))
+    loc = np.where(res >= 0.95)
+    if not np.any(loc):
+        continue
+    (minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(res)
+    (startX, startY) = maxLoc
+    endX = startX + item.shape[1]
+    endY = startY + item.shape[0]
+    cv2.rectangle(ss_cropped, (startX, startY), (endX, endY), (255, 0, 0), 3)
+    found_items.append(f"{str(_item.stem)}")
+
+found_items.sort()
+print(found_items)
+cv2.imshow("test", ss_cropped)
+cv2.waitKey()
